ab17009cf4fa3e9f53cba8420f48672a93b36c58
William Ang <williamang9174@gmail.com>
2022-07-17 13:34:17 +0800

    add backend password validation

--- backend/app/schemas/auth.py
+++ backend/app/schemas/auth.py
@@ -1,13 +1,39 @@
-from pydantic import BaseModel, HttpUrl
-
-from typing import Sequence
+from pydantic import BaseModel, validator
 
 
 class UserBase(BaseModel):
     email: str
 
 
+special_character = "!@#$%^&*"
+
+
 class UserCreate(UserBase):
+    @validator('password')
+    def validate_password(cls, v):
+        if len(v) < 8:
+            raise ValueError(
+                'Password length must be more than or equal to 8 characters')
+        if len(v) > 100:
+            raise ValueError(
+                'Password length must be less than or equal to 100 characters')
+        if not any(char.isdigit() for char in v):
+            raise ValueError(
+                'Password must contain at least 1 number')
+        if not any(char.isupper() for char in v):
+            raise ValueError(
+                'Password must contain at least 1 uppercase letter'
+            )
+        if not any(char.islower() for char in v):
+            raise ValueError(
+                'Password must contain at least 1 lowercase letter'
+            )
+        if not any(char in special_character for char in v):
+            raise ValueError(
+                f'Password must contain at least one of the special characters {special_character}'
+            )
+        return v
+
     password: str
 
 

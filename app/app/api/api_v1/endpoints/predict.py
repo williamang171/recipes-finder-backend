f8a51bf83d525ba60d59e4f3c173849399fc31c8
William Ang <williamang9174@gmail.com>
2022-12-26 15:37:48 +0800

    update predict api response shape

--- app/app/api/api_v1/endpoints/predict.py
+++ app/app/api/api_v1/endpoints/predict.py
@@ -1,7 +1,7 @@
 from fastapi import Depends, APIRouter, UploadFile
 import os
 
-from app.schemas.predict import PredictViaUrl
+from app.schemas.predict import PredictViaUrl, PredictResult
 from app.api.deps import get_settings
 from app import config
 from app.api.deps import verify_recaptcha
@@ -12,16 +12,16 @@ sample_image_path = os.path.join(os.getcwd(), 'app', 'static', 'ramen.jpeg')
 api_router = APIRouter()
 
 
-@api_router.post("/")
-def predict_via_url(*, predict_via_url: PredictViaUrl, settings: config.Settings = Depends(get_settings), valid_recaptcha: bool = Depends(verify_recaptcha)):
+@api_router.post("/", response_model=list[PredictResult])
+def predict_via_url(*, predict_via_url: PredictViaUrl, settings: config.Settings = Depends(get_settings)):
     if (settings.USE_CLARIFAI == 'True'):
         return predict_with_clarifai(settings=settings, image_url=predict_via_url.url)
     result = query(predict_via_url.url, settings.HUGGINGFACE_TOKEN)
     return result
 
 
-@api_router.post("/upload")
-async def predict_via_upload(*, file: UploadFile, settings: config.Settings = Depends(get_settings), valid_recaptcha: bool = Depends(verify_recaptcha)):
+@api_router.post("/upload", response_model=list[PredictResult])
+async def predict_via_upload(*, file: UploadFile, settings: config.Settings = Depends(get_settings)):
     if (settings.USE_CLARIFAI == 'True'):
         file_bytes = await file.read()
         return predict_with_clarifai(settings=settings, file_bytes=file_bytes)

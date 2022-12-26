663798a990db537e93539bda4f574d0ecd76d3b3
William Ang <williamang9174@gmail.com>
2022-12-26 14:14:47 +0800

    update requirements.txt

--- app/app/api/hugging_face_utils.py
+++ app/app/api/hugging_face_utils.py
@@ -1,9 +1,8 @@
 import requests
 import json
-from transformers import pipeline
 
-classifier = pipeline("image-classification",
-                      model="william7642/my_awesome_food_model")
+# classifier = pipeline("image-classification",
+#                       model="william7642/my_awesome_food_model")
 
 API_URL = "https://api-inference.huggingface.co/models/william7642/my_awesome_food_model"
 
